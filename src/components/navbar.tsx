5c462d1f4eed7c8f3dee0e2496a02fa11fe32a9a
Osama Jandali <osamajandaly@gmail.com>
2025-01-15 15:11:39 +0200

    Update nav links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,16 @@
 import Logo from "../assets/logo.svg";
-import { Button } from "@/components/ui/button";
 const Navbar: React.FC = () => {
   return (
     <nav className="py-7 border-border border-b">
       <div className="flex justify-between items-center max-w-5xl mx-auto">
         <div>
-          <a href="/">
+          <a href="https://if.greensoftware.foundation/" target="_blank" rel="noopener noreferrer">
             <img src={Logo} alt="Logo" className="h-10" />
           </a>
         </div>
-        <div>
-          <Button>Become a watcher</Button>
+        <div className="flex gap-4">
+          <a href="/" >Home</a>
+          <a href="https://if.greensoftware.foundation/intro" target="_blank" rel="noopener noreferrer" >Documentation</a>
         </div>
       </div>
     </nav>
